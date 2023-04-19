6ecc00efce20db352a3d09a0bded0a07a45169a6
Gromovoi Stanislav <gromila12@rambler.ru>
2023-04-19 09:45:12 +0600

    mongoose-start

---
+++ mongoose-start/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(`'mongodb://localhost/demo')
+  ],
+})
+export class AppModule {}
